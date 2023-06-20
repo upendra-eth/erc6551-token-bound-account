d9b83b62a0d1d22d83efc0c794a42e745ad29499
UPENDRA SINGH <upendra.singh@antiersolutions.com>
2023-06-20 16:07:50 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,23 @@
+# What is ERC-6551?
+ERC-6551 is proposed system of smart contracts that gives every NFT it's own wallet address. This allows all NFTs to own assets, interact with applications, and participate as sovereign agents in the Ethereum ecosystem.
+
+ERC-6551 is unique in it's approach because it requires zero changes to existing NFT smart contracts or infrastructure.
+
+Read more about ERC-6551 here(opens in a new tab).
+https://eips.ethereum.org/EIPS/eip-6551
+
+
+
+# Sample Hardhat Project
+
+This project demonstrates a basic Hardhat use case. It comes with a sample contract, a test for that contract, and a script that deploys that contract.
+
+Try running some of the following tasks:
+
+```shell
+npx hardhat help
+npx hardhat test
+REPORT_GAS=true npx hardhat test
+npx hardhat node
+npx hardhat run scripts/deploy.js
+```
---
+++ scripts/deploy.js
@@ -0,0 +1,28 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+async function main() {
+  const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+    value: lockedAmount,
+  });
+
+  await lock.waitForDeployment();
+
+  console.log(
+    `Lock with ${ethers.formatEther(
+      lockedAmount
+    )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  );
+}
+
+// We recommend this pattern to be able to use async/await everywhere
+// and properly handle errors.
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
